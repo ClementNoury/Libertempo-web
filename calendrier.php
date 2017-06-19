d7eb675cfd6f1581e9875fcec6705b1bbb5dfa48
Prytoegrian <prytoegrian@protonmail.com>
2017-06-19 22:55:24 +0200

    WIP - récupération token à la co en cours

--- calendrier.php
+++ calendrier.php
@@ -5,14 +5,14 @@ require_once ROOT_PATH . 'define.php';
 $session=(isset($_GET['session']) ? $_GET['session'] : ((isset($_POST['session'])) ? $_POST['session'] : session_id()) ) ;
 $session = htmlentities($session, ENT_QUOTES | ENT_HTML401);
 
-$api = new \App\Libraries\ApiClient();
-
 
 include_once ROOT_PATH .'fonctions_conges.php';
 include_once INCLUDE_PATH .'fonction.php';
 header_menu('', 'Libertempo : '._('calendrier_titre'));
 
-d($api->get('hello_world'));
+// il faut chiffrer les mdp avec le meme algo, quel que soit le how_to_connect_user
+
+d($api->authentification('hr', 'none'));
 echo (new \App\ProtoControllers\Calendrier())->get();
 
 bottom();
